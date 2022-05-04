abd7e68e9315b87e79b80d0e5c38854b9827f021
fco-junior <cassemirojunior@alu.ufc.br>
2022-05-04 17:41:21 -0300

    style in list

--- src/components/BuyList.vue
+++ src/components/BuyList.vue
@@ -2,7 +2,7 @@
     <div class="buy-list">
         <h4>{{ msg }}</h4>
         <v-InputText type="text" v-model="value"/>
-        <v-Button icon="pi pi-check" iconPos="center" @click="save()" />
+        <v-Button class="button" icon="pi pi-check" iconPos="center" @click="save()" />
 
         <div class="items" v-for="(item, index) in items" :key="index">
             <label class="item-name">{{ item.value }}</label>
@@ -25,10 +25,23 @@ export default {
         };
     },
 
-    
+    methods: {
+        save() {
+            this.items.push({
+                value: this.value                
+            });
+            this.value = "";
+        }
+    }
 }
 </script>
 
 <style scoped>
 
+.button {
+  margin-left: 5px;
+}
+.items{
+  margin: 15px;
+}
 </style>
