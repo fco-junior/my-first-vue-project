32c84987345ef7c2de71bd38aaaf4f2220270983
fco-junior <cassemirojunior@alu.ufc.br>
2022-05-04 16:39:42 -0300

    create component BuyList

---
+++ src/components/BuyList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="buy-list">
+        <h4>{{ msg }}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BuyList',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
