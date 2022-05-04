f0b7aad04d17a7a2a1b04a64f0ca1f0664a6dfae
fco-junior <cassemirojunior@alu.ufc.br>
2022-05-04 16:46:59 -0300

    delete HelloWorld component and import BuyList in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BuyList msg="Buy List"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BuyList from './components/BuyList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BuyList
   }
 }
 </script>
