10ce6f62be92a0dc00f4175e4de8756a8602bc15
fco-junior <cassemirojunior@alu.ufc.br>
2022-05-04 17:05:34 -0300

    create function data for save items

--- src/components/BuyList.vue
+++ src/components/BuyList.vue
@@ -2,15 +2,23 @@
     <div class="buy-list">
         <h4>{{ msg }}</h4>
         <v-InputText />
-        <v-Button icon="pi pi-check" iconPos="center"></v-Button>
+        <v-Button icon="pi pi-check" iconPos="center" @click="save()></v-Button>
     </div>
 </template>
 
 <script>
 export default {
     name: 'BuyList',
+    
     props: {
         msg: String
+    },
+
+    data: function () {
+        return {
+            value: "",
+            items: [],
+        };
     }
 }
 </script>
