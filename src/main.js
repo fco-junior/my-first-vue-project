8db9513f39dc018a62aff710512b2fc1ad8fd97b
fco-junior <cassemirojunior@alu.ufc.br>
2022-05-04 16:28:28 -0300

    primevue component imports

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config'
-const app = createApp(App)
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Checkbox from 'primevue/checkbox';
 
 import 'primevue/resources/themes/saga-blue/theme.css';       //theme
 import 'primevue/resources/primevue.min.css';                 //core css
 import 'primeicons/primeicons.css';                           //icons
 
-app.use(PrimeVue)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(PrimeVue);
+app.component('v-InputText', InputText);
+app.component('v-Button', Button);
+app.component('v-Checkbox', Checkbox);
+
+app.mount('#app');
