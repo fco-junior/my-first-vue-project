a0af77f121d3d6e016b6b703057bfecd65150aec
fco-junior <cassemirojunior@alu.ufc.br>
2022-05-04 16:01:40 -0300

    primevue configuration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+const app = createApp(App)
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';       //theme
+import 'primevue/resources/primevue.min.css';                 //core css
+import 'primeicons/primeicons.css';                           //icons
+
+app.use(PrimeVue)
+
+app.mount('#app')
