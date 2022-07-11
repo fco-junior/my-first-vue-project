e5adf0aa8c51b4abc14224f3b48e7b6e0f47485b
fco-junior <cassemirojunior16@gmail.com>
2022-07-11 00:05:20 -0300

    Adding action buttons and adjusting styles on the Product Details page.

--- src/components/pages/ProductDetails.vue
+++ src/components/pages/ProductDetails.vue
@@ -1,38 +1,69 @@
 <template>
   <div class="product-details">
     <Toast />
-    <Card class="card">
+    <Card>
       <template #title> Product Details </template>
+
       <template #content>
-        <div class="p-card-content">
-          <P>
-            ID: <strong>{{ product.id }}</strong>
-          </P>
-          <P>
-            Name: <strong>{{ product.name }}</strong>
-          </P>
-          <P>
-            Description: <strong>{{ product.description }}</strong>
-          </P>
-          <P>
-            Price: <strong>R$ {{ product.price }}</strong>
-          </P>
-          <P>
-            Active: <strong>{{ isActive }}</strong>
-          </P>
-          <P>
-            Created Date: <strong>{{ product.createdDate }}</strong>
-          </P>
-          <P>
-            Updated Date: <strong>{{ product.updatedDate }}</strong>
-          </P>
-        </div>
+        <span>
+          ID: <strong>{{ product.id }}</strong>
+        </span>
+        <span>
+          Name: <strong>{{ product.name }}</strong>
+        </span>
+        <span>
+          Description: <strong>{{ product.description }}</strong>
+        </span>
+        <span>
+          Price: <strong>R$ {{ product.price }}</strong>
+        </span>
+        <span>
+          Active: <strong>{{ isActive }}</strong>
+        </span>
+        <span>
+          Created Date: <strong>{{ product.createdDate }}</strong>
+        </span>
+        <span>
+          Updated Date: <strong>{{ product.updatedDate }}</strong>
+        </span>
       </template>
+
       <template #footer>
-        <div class="p-card-footer">
-          <RouterLink class="link" to="/">
-            <Button class="p-button p-button-raised" label="Back" />
-          </RouterLink>
+        <RouterLink class="link" to="/">
+          <Button
+            class="p-button-rounded p-button-raised p-button-success"
+            icon="pi pi-table"
+            v-tooltip.bottom="'Go to Product Management'"
+          />
+        </RouterLink>
+
+        <div class="action-buttons">
+          <Button
+            class="p-button-rounded p-button-raised"
+            icon="pi pi-pencil"
+            v-tooltip.bottom="'Uptade product'"
+          />
+
+          <Button
+            v-show="isActive === 'Yes'"
+            class="p-button-rounded p-button-raised p-button-secondary"
+            icon="pi pi-eye-slash"
+            v-tooltip.bottom="'Inactive product'"
+          />
+
+          <Button
+            v-show="isActive === 'No'"
+            class="p-button-rounded p-button-raised p-button-secondary"
+            icon="pi pi-eye"
+            v-tooltip.bottom="'Active product'"
+          />
+
+          <Button
+            v-show="isActive === 'No'"
+            class="p-button-rounded p-button-raised p-button-danger"
+            icon="pi pi-trash"
+            v-tooltip.bottom="'Delete product'"
+          />
         </div>
       </template>
     </Card>
