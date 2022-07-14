b88868fc94442722312399f633bc1252dd4a471a
fco-junior <cassemirojunior16@gmail.com>
2022-07-14 13:49:26 -0300

    Creating TabViewProductTable component for implementing TabView in product table.

---
+++ src/components/TabViewProductTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <TabView class="tabview-custom">
+    <TabPanel>
+      <template #header>
+        <i class="pi pi-eye" />
+        <span>Actives products</span>
+      </template>
+    </TabPanel>
+
+    <TabPanel>
+      <template #header>
+        <i class="pi pi-eye-slash" />
+        <span>Inactives products</span>
+      </template>
+    </TabPanel>
+  </TabView>
+</template>
+
+<script>
+export default {
+  name: 'TabViewProductTable',
+};
+</script>
