9919d8e6181e9c271d2fab72bc6af6a3fe7e2e69
fco-junior <cassemirojunior16@gmail.com>
2022-07-18 23:14:34 -0300

    Test error to trying get all products.

--- tests/unit/mocks/ProductManagementMock.js
+++ tests/unit/mocks/ProductManagementMock.js
@@ -1,26 +1,36 @@
-export const mockResponseGetAllProducts = {
-    status: 200,
+export const mockSuccessGetAllProducts = {
+  status: 200,
+  data: {
+    data: [
+      {
+        id: 2,
+        name: 'Bean',
+        description: 'Description',
+        price: 8.5,
+        isActive: true,
+        createdDate: '2022-07-14T12:06:50.498Z',
+        updatedDate: '2022-07-14T12:06:50.498Z'
+      },
+      {
+        id: 1,
+        name: 'Rice',
+        description: 'Description',
+        price: 7.5,
+        isActive: true,
+        createdDate: '2022-07-14T12:06:50.498Z',
+        updatedDate: '2022-07-14T12:06:50.498Z'
+      }
+    ],
+    errors: []
+  }
+};
+
+export const mockErrorGetAllProducts = {
+  response: {
+    status: 400,
     data: {
-      data: [
-        {
-          id: 2,
-          name: 'Bean',
-          description: 'Description',
-          price: 8.5,
-          isActive: true,
-          createdDate: '2022-07-14T12:06:50.498Z',
-          updatedDate: '2022-07-14T12:06:50.498Z'
-        },
-        {
-          id: 1,
-          name: 'Rice',
-          description: 'Description',
-          price: 7.5,
-          isActive: true,
-          createdDate: '2022-07-14T12:06:50.498Z',
-          updatedDate: '2022-07-14T12:06:50.498Z'
-        }
-      ],
+      data: [],
       errors: []
     }
-  };
+  }
+};
--- tests/unit/pages/ProductManagement.spec.js
+++ tests/unit/pages/ProductManagement.spec.js
@@ -3,12 +3,11 @@ import plugins from '../plugins';
 import { config, flushPromises, mount } from '@vue/test-utils';
 import ProductManagement from '../../../src/components/pages/ProductManagement.vue';
 
-import {
-  getAllProducts
-} from '../../../src/services/productService';
+import { getAllProducts } from '../../../src/services/productService';
 
 import {
-  mockResponseGetAllProducts
+  mockSuccessGetAllProducts,
+  mockErrorGetAllProducts
 } from '../mocks/ProductManagementMock';
 
 config.global = setup.global;
@@ -18,11 +17,11 @@ jest.mock('../../../src/services/productService');
 describe('ProductManagement.vue', () => {
   test('Get all products', async () => {
     //mock
-    getAllProducts.mockResolvedValueOnce(mockResponseGetAllProducts);
+    getAllProducts.mockResolvedValueOnce(mockSuccessGetAllProducts);
     //build
     const wrapper = mount(ProductManagement, {
       global: {
-        plugins,
+        plugins
       }
     });
     //exec
@@ -30,5 +29,21 @@ describe('ProductManagement.vue', () => {
     let results = wrapper.vm.products;
     //expect
     expect(results).toHaveLength(2);
+  });
+
+  test('Error to get all products', async () => {
+    //mock
+    getAllProducts.mockRejectedValueOnce(mockErrorGetAllProducts);
+    //build
+    const wrapper = mount(ProductManagement, {
+      global: {
+        plugins
+      }
+    });
+    //exec
+    await flushPromises();
+    let results = wrapper.vm.products;
+    //expect
+    expect(results).toHaveLength(0);
   });
 });
