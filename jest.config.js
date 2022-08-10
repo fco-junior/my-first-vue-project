adbe89708dd8e1a2ebfea3ca6cd98aa1efdc669c
fco-junior <cassemirojunior16@gmail.com>
2022-08-10 14:34:59 -0300

    Adjustments in jest.config.js file.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue'
+  ],
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov', 'text', 'text-summary']
 }
